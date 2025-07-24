136e59a8517330e7511f1f5943ed96c4223bc41d
Attila Bulenda <36601162+attila-bulenda@users.noreply.github.com>
2025-07-24 14:48:33 +0200

    Adding some notifications

---
+++ AirlineBookingSystem/AirlineBookingSystem.Notifications.Application/Notifications/Notifications.cs
@@ -0,0 +1,30 @@
+﻿namespace AirlineBookingSystem.Notifications.Application.Notifications
+{
+    public static class Notifications
+    {
+        public static void FlightBookingNotification(string email, string flight)
+        {
+            Console.WriteLine($"Flight {flight} has been booked and confirmation sent to {email}. Thank you for choosing our airline.");
+        }
+
+        public static void FlightUpdateNotification(string flight)
+        {
+            Console.WriteLine($"Flight {flight} has been changed. Please contact our customer service for further information.");
+        }
+
+        public static void FlightCancellationNotification(string flight)
+        {
+            Console.WriteLine($"Flight {flight} has been cancelled. Please contact our customer service for further information.");
+        }
+
+        public static void UserRegistrationNotification(string email)
+        {
+            Console.WriteLine($"User with {email} has been created. Thank you for joining our customer base.");
+        }
+
+        public static void UserDeletionNotification(string email)
+        {
+            Console.WriteLine($"User with {email} has been deleted. Hope to see you soon again!");
+        }
+    }
+}
