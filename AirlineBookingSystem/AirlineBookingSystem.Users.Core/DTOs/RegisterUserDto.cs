5ed177fed0520dfbad4a9765558a6f65230077e0
Attila Bulenda <36601162+attila-bulenda@users.noreply.github.com>
2025-07-24 09:23:30 +0200

    Adding some restrictions on the DTO about PW length and EmailAddress verification

--- AirlineBookingSystem/AirlineBookingSystem.Users.Core/DTOs/RegisterUserDto.cs
+++ AirlineBookingSystem/AirlineBookingSystem.Users.Core/DTOs/RegisterUserDto.cs
@@ -5,6 +5,7 @@ namespace AirlineBookingSystem.Users.Core.DTOs
     public class RegisterUserDto: SystemUserDto
     {
         [Required]
+        [StringLength(15, ErrorMessage = "Your Password is limited to {2} to {1} characters", MinimumLength = 6)]
         public string Password { get; set; }
     }
 }
--- AirlineBookingSystem/AirlineBookingSystem.Users.Core/DTOs/SystemUserDto.cs
+++ AirlineBookingSystem/AirlineBookingSystem.Users.Core/DTOs/SystemUserDto.cs
@@ -1,10 +1,18 @@
-﻿namespace AirlineBookingSystem.Users.Core.DTOs
+﻿using DataAnnotationsExtensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace AirlineBookingSystem.Users.Core.DTOs
 {
     public class SystemUserDto
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
+        [Email]
         public string Email { get; set; }
+        [Required]
         public string UserName { get; set; }
     }
 }
